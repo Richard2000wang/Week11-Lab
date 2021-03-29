4f4efb25bb166118d9299e394b5df0482bf9b894
Richard Wang <richard2000wang@outlook.com>
2021-03-28 21:06:06 -0700

    Add array

---
+++ JS/app.js
@@ -0,0 +1,19 @@
+let Prep = {
+  todo: "prepare ingredients",
+  status: "complete"
+}
+let Tools = {
+  todo: "take out relevent pots, pans, and utensils",
+  status: "complete"
+}
+let Cook = {
+  todo: "cook the food",
+  status: "incomplete"
+}
+let Serve = {
+  todo: "platter the food and serve",
+  status: "incomplete"
+}
+
+let todolist = [Prep, Tools, Cook, Serve];
+let completedTodos = [];
