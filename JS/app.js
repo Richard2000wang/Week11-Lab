0815eb3c2e9a8cba82cbd7e59a84bc839ca7090e
Richard Wang <richard2000wang@outlook.com>
2021-03-28 20:55:00 -0700

    Add for loop to log todo key

--- JS/app.js
+++ JS/app.js
@@ -2,27 +2,35 @@ let Prep = {
     todo: "prepare ingredients",
     status: "complete"
   }
-  let Tools = {
+let Tools = {
     todo: "take out relevent pots, pans, and utensils",
     status: "complete"
   }
-  let Cook = {
+let Cook = {
     todo: "cook the food",
     status: "incomplete"
   }
-  let Serve = {
+let Serve = {
     todo: "platter the food and serve",
     status: "incomplete"
   }
   
-  let todolist = [Prep, Tools, Cook, Serve];
-  let completedTodos = [];
+let todolist = [Prep, Tools, Cook, Serve];
+let completedTodos = [];
   
-  var arraylength = todolist.length;
+var arraylength = todolist.length;
   
-  for (var i = 0; i < arraylength; i++) {
+for (var i = 0; i < arraylength; i++) {
     if (Object.values(todolist[i]).indexOf('complete') > -1) {
         completedTodos.push(todolist[i]);
     }
   };
   
+var completelength = completedTodos.length
+
+for (var o = 0; o < completelength; o++) {
+    let {
+        todo
+    } = completedTodos[o]
+      console.log(todo);
+}
